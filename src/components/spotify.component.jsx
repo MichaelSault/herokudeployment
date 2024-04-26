198b901f3d8253c4131bce480b53148dc592cf9b
MichaelSault <michael_sault@hotmail.com>
2024-04-25 23:29:13 -0400

    added fetch to call spotify dev and set access token variable

--- src/components/spotify.component.jsx
+++ src/components/spotify.component.jsx
@@ -1,9 +1,29 @@
 import '../App.css';
-import { Container, InputGroup, FormControl, Button, Row, Cards, Form } from 'react-bootstrap';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
+
+//react has a built in dotenv, so we do not have to install it manually
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_ID;
+const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_SECRET;
+
 
 function Spotify() {
     const [searchInput, setSearchInput] = useState("");
+    const [accessToken, setAccessToken] = useState("");
+
+    useEffect(() => {
+        // API Access Token
+        var authParameters = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+        }
+        fetch('https://accounts.spotify.com/api/token', authParameters)
+            .then(result => result.json())
+            .then(data => setAccessToken(data.access_token));
+    }, []);
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -36,6 +56,17 @@ function Spotify() {
                             Search
                         </Button>
                     </InputGroup>
+                </Container>
+                <Container>
+                    <Row className='mx-2 row row-cols-4'>
+                        <Card>
+                            <Card.Img src="#" />
+                            <Card.Body>
+                                <Card.Title>Album Name</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                    
                 </Container>
             </div>
         </>
